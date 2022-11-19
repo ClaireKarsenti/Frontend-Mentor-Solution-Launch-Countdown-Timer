d82ed82321e7330cc9e7ad275c041b148cf12f2b
Claire Karsenti <claire.karsenti.web@gmail.com>
2022-11-19 16:26:37 +0100

    :pencil2: Updated deadLine for CountDown

--- src/components/CountDown.component.tsx
+++ src/components/CountDown.component.tsx
@@ -7,18 +7,20 @@ const MINUTE = SECOND * 60;
 const HOUR = MINUTE * 60;
 const DAY = HOUR * 24;
 
-const CountDown = ({ deadline = 'December, 31, 2022' }) => {
-  const parsedDeadline = useMemo(() => Date.parse(deadline), [deadline]);
-  const [time, setTime] = useState(parsedDeadline - Date.now());
+const CountDown = ({ timeToDays = 9 * DAY }) => {
+  const deadLine = useMemo(() => timeToDays, [timeToDays]);
+  const [time, setTime] = useState<number>(deadLine);
+
+  const countDownDate = new Date().getTime() + deadLine;
 
   useEffect(() => {
     const interval = setInterval(
-      () => setTime(parsedDeadline - Date.now()),
+      () => setTime(countDownDate - Date.now()),
       1000
     );
 
     return () => clearInterval(interval);
-  }, [parsedDeadline]);
+  }, [deadLine]);
 
   return (
     <CountDownContainer>
